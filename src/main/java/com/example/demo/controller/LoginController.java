37678029927fd5cd1e6c274340f395a4f1fed621
shun14-tech <ikeike07140714@gmail.com>
2026-06-27 12:07:13 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String index(){
+        return "login";
+    }
+
+    @GetMapping("/login")
+    public String showloginform(){
+        return "login";
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/securityconfig/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.example.demo.securityconfig;
+
+import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+        http
+        .authorizeHttpRequests(authorize->authorize
+        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+        .requestMatchers("/login","/").permitAll()
+        .requestMatchers("/admin/register","/admin/rentals").hasRole("ADMIN")
+        .anyRequest().authenticated()
+        ).formLogin(form->form
+            .loginPage("/login")
+            .usernameParameter("username")
+            .passwordParameter("password")
+            .defaultSuccessUrl("/items",true)
+            .failureUrl("/login?error")
+        ).logout(logout->logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/logout?logout")
+            .permitAll()
+        ).csrf(Customizer.withDefaults());
+        return http.build();
+        
+    }
+
+    @Bean
+    public BCryptPasswordEncoder PasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
