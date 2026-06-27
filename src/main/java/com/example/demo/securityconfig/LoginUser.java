d847a15b81bf909b2486cd502033b2b212c5dfa7
shun14-tech <ikeike07140714@gmail.com>
2026-06-27 14:26:05 +0900

    LoginUser 追記

--- src/main/java/com/example/demo/securityconfig/LoginUser.java
+++ src/main/java/com/example/demo/securityconfig/LoginUser.java
@@ -1,5 +1,57 @@
 package com.example.demo.securityconfig;
 
-public class LoginUser {
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class LoginUser implements UserDetails{
+
+    private int userId;
+    private String mailaddress;
+    private String password;
+    private String name;
+
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public LoginUser(int userId,String mailaddress,String password,String name,Collection<? extends GrantedAuthority>authorities){
+        this.userId=userId;
+        this.mailaddress=mailaddress;
+        this.password=password;
+        this.authorities=authorities;
+        this.name=name;
+    }
+
+    
+   @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+   public int getUserId(){
+        return userId;
+    }
+   
+    public User getUser(){
+        return user;
+    }
+
+    @Override
+    public String getUsername() {
+        return mailaddress; 
+    }
+
+    @Override
+    public String getPassword() {
+        return password; 
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+    @Override
+    public boolean isEnabled() { return true; }
 
 }
